0fbdc1fb4c555d661d8ce8f6ba49766e68b426da
Amanda Pennell <31744744+amandapennell@users.noreply.github.com>
2023-12-16 20:29:12 -0400

    Add sortee array; create functions to sort and add to list

--- 16-naughty-list-nice-list/index.js
+++ 16-naughty-list-nice-list/index.js
@@ -10,3 +10,62 @@ const addBtn = document.getElementById("add-btn")
 sortBtn.addEventListener("click", sortList)
 resetBtn.addEventListener("click", resetList)
 addBtn.addEventListener("click", addToList)
+
+const sorteesArr = [
+    { name: "David", hasBeenGood: false },
+    { name: "Del", hasBeenGood: true },
+    { name: "Valerie", hasBeenGood: false },
+    { name: "Astrid", hasBeenGood: true }
+]
+
+function updateLists() {
+    niceList.innerHTML = ""
+    naughtyList.innerHTML = ""
+
+    for (let i = 0; i < sorteesArr.length; i++) {
+        const listItem = document.createElement("li")
+        listItem.textContent = sorteesArr[i].name
+
+        if (sorteesArr[i].hasBeenGood) {
+            niceList.appendChild(listItem)
+        } else {
+            naughtyList.appendChild(listItem)
+        }
+    }
+}
+
+function addToList(event) {
+    event.preventDefault()
+
+    const personName = {
+        name: name.value,
+        hasBeenGood: !naughty.checked
+    };
+
+    sorteesArr.push(personName)
+    updateLists()
+    name.value = ""
+    naughty.checked = false
+}
+
+function resetList() {
+    niceList.innerHTML = ""
+    naughtyList.innerHTML = ""
+}
+
+function sortList() {
+    // Sorting is done dynamically when updating the lists in the updateLists function
+    updateLists()
+}
+
+
+/** Challenge: 
+  - Write the JavaScript to sort the people in sorteesArr into the naughty and nice lists, 
+  according to whether they have been good or not. 
+  Then display the names in the relevant place in the DOM.
+**/
+
+/** Stretch goals:
+  - Add the option to add new names to the sorteesArr.
+  - Make it possible to switch people to the other list.
+**/
